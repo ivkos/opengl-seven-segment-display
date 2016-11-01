6849bbc9486ec79fd5c6d33194d23e722e8c6356
Ivaylo Stoyanov <me@ivkos.com>
2016-11-01 23:31:17 +0200

    Use first cmdline param for number

--- src/main/java/com/ivkos/opengl/Application.java
+++ src/main/java/com/ivkos/opengl/Application.java
@@ -4,11 +4,20 @@ import com.ivkos.opengl.sevensegmentdisplay.SevenSegmentDisplayDrawable;
 import com.ivkos.opengl.util.GlDrawable;
 import com.ivkos.opengl.util.GlWindow;
 
+import static java.lang.Integer.parseInt;
+
 public class Application
 {
    public static void main(String[] args)
    {
-      GlDrawable drawable = new SevenSegmentDisplayDrawable(1337, "#7EFF00");
+      int number;
+      try {
+         number = parseInt(args[0]);
+      } catch (NumberFormatException | IndexOutOfBoundsException e) {
+         number = 1337;
+      }
+
+      GlDrawable drawable = new SevenSegmentDisplayDrawable(number, "#7EFF00");
       GlWindow ctx = new GlWindow("Seven Segment Display", 720, 720, drawable);
 
       new Thread(ctx).start();
